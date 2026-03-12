b46bde42ad337a41d8b3d4a9f920be7e7925735e
anushka-a1 <anushkachaurasia191@gmail.com>
2026-03-12 14:45:42 +0530

    Refactor Selenium script to print Wikipedia title
    Simplified the Selenium script by removing unnecessary refresh and navigation steps.

--- Practice/task1(12-3-26).py
+++ Practice/task1(12-3-26).py
@@ -1,19 +1,11 @@
-# open wikipedia refresh fetch title, open amazon fetch title go back close
-
+""" Task 1 - write a selenium script to open a website and print the page title """
 from time import sleep
 from selenium.webdriver import Chrome,ChromeOptions
-o = ChromeOptions()
-o.add_experimental_option("detach", True)
-driver = Chrome(options=o)
+o=ChromeOptions()
+o.add_experimental_option("detach",True)
+driver=Chrome(options=o)
 driver.get("https://www.wikipedia.org/")
-sleep(5)
-driver.refresh()
-sleep(5)
+sleep(3)
 print(driver.title)
-
-driver.get("https://www.amazon.com/")
-sleep(5)
-print(driver.title)
-driver.back()
-sleep(5)
 driver.close()
+
